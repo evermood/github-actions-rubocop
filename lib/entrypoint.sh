b51709c84330b3f4ed20c9208b79b73a7061703d
Tim Moser <tim.moser@zweitag.de>
2020-09-07 15:14:33 +0200

    Update gemfile versions.

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gem install rubocop -v 0.80.1
-gem install rubocop-rspec -v 1.38.1
-gem install rubocop-performance -v 1.5.2
-gem install rubocop-rails -v 2.4.2
+gem install rubocop -v 0.90.0
+gem install rubocop-rspec -v 1.43.2
+gem install rubocop-performance -v 1.8.0
+gem install rubocop-rails -v 2.8.0
 
 ruby /action/lib/index.rb
